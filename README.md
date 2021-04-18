8ce1ab17c19c500efede9c9fa9a698296e8c25aa
smc181002 <meherchaitanya18802@gmail.com>
2021-04-18 20:26:32 +0530

    release 1.0

---
+++ README.md
@@ -0,0 +1,22 @@
+Skt-chat-17
+===========
+
+Installation
+------------
+
+```sh
+git clone https://github.com/smc181002/skt-chat-17.git
+```
+
+What to use?
+------------
+
+If you are using a `linux` or `mac os` machine, run the linux_client.py
+
+If you are using `windows` machine, run the windows_client.py
+
+How to use?
+-----------
+
+when running the python file, the sender should provide the IP as argument of the
+receiver and ensure that there is connection between both the messaging clients
---
+++ windows_client.py
@@ -0,0 +1,43 @@
+import socket, threading, os, sys
+
+skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+ip, port = "", 69
+
+skt.bind((ip, port))
+
+msgs = []
+
+def print_msgs():
+    os.system("cls")
+    for msg in msgs:
+        print(f"{msg['name']}  |> {msg['msg']}")
+    print()
+
+def send_msg():
+    try:
+        ip = sys.argv[1]
+    except:
+        pass
+
+    while True:
+        msg = input("Enter your message: ")
+        if msg == "/exit":
+            os._exit(1)
+
+        skt.sendto(msg.encode(),(ip, port))
+        msgs.append({"name": "you", "msg": msg})
+        print_msgs()
+
+def recv_msg():
+    while True:
+        data = skt.recvfrom(2048)
+
+        msgs.append({"name": data[1][0], "msg": data[0].decode()})
+        print_msgs()
+
+thread_send = threading.Thread(target=send_msg)
+thread_send.start()
+
+thread_recv = threading.Thread(target=recv_msg)
+thread_recv.start()
